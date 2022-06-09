df84ce7f9cfd3432816225009c3df7483637200c
Sobel <sandrasobel95@gmail.com>
2022-06-10 00:58:28 +0100

    6.2.-Pierwszy-test-jednostkowy

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.user;
+
+public class Calculator {
+    public int a;
+    public int b;
+
+    public int add(int a,int b){
+        return a + b;
+    }
+    public int substract(int a, int b){
+        return a - b;
+    }
+}
