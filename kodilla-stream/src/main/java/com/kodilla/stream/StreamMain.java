f96e5dc02ac4df56d130624c7e6850652a217473
Sobel <sandrasobel95@gmail.com>
2022-07-09 15:35:41 +0100

    poprawa - 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -11,7 +12,13 @@ public class StreamMain {
         Forum forum = new Forum();
         Map<Integer,ForumUser> theResultMapOfUsers = forum.getUserList().stream()
                 .filter(user->user.getSex()=='M')
-                .filter(date->date.getYearOfBirth()<=2002)
+                .filter(date->
+                        (date.getYearOfBirth() <= new Date().getYear()+2001-122)    ||
+                        (  (date.getYearOfBirth() == new Date().getYear()+2002-122) &&
+                           (date.getMonthOfBirth() <= new Date().getMonth()+1)    ) ||
+                        (  ((date.getYearOfBirth() == new Date().getYear()+2002-122) &&
+                           (date.getMonthOfBirth() == new Date().getMonth()+1 ))  &&
+                           (date.getDayOfBirth() <= new Date().getDate() ) )  )
                 .filter(posts->posts.getPostsQuantity()>=1)
                 .collect(Collectors.toMap(ForumUser::getID,forumuser->forumuser));
 
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -9,10 +9,12 @@ public final class Forum {
         theForumUserList.add(new ForumUser(1001,"user1",'M',1995,10,5,0));
         theForumUserList.add(new ForumUser(1002,"user2",'F',1998,9,16,7));
         theForumUserList.add(new ForumUser(1004,"user3",'M',1999,2,2,2));
-        theForumUserList.add(new ForumUser(1005,"user4",'F',2002,4,28,0));
+        theForumUserList.add(new ForumUser(1005,"user4",'F',2001,4,28,1));
         theForumUserList.add(new ForumUser(1006,"user5",'M',2001,11,12,4));
         theForumUserList.add(new ForumUser(1007,"user6",'F',1993,12,18,5));
         theForumUserList.add(new ForumUser(1008,"user7",'M',1991,3,31,1));
+        theForumUserList.add(new ForumUser(1010,"user8",'M',2002,8,7,2));
+        theForumUserList.add(new ForumUser(1011,"user9",'M',2002,7,11,7));
 
     }
     public List<ForumUser> getUserList() {
